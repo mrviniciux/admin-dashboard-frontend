3f32eda51dead8d3b85d436b863fd9099489ac99
mrviniciux <httpmarks@gmail.com>
2024-11-06 20:11:38 -0300

    feat(approutercache): added AppRouterCacheProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { theme } from '@/config';
 import { AppProvider } from '@toolpad/core/AppProvider';
 import { NAVIGATION } from '@/config/navigation';
 import Auth from './Auth';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 
 type AppPageProps = {
   children: ReactNode;
@@ -15,25 +16,26 @@ type AppPageProps = {
 
 export default function RootLayout({ children }: AppPageProps) {
   const queryClient = new QueryClient();
-
   return (
     <html lang="pt-BR">
       <head></head>
       <body>
-        <AppProvider
-          branding={{ title: 'admin-dashboard-frontend' }}
-          navigation={NAVIGATION}
-          theme={theme}
-        >
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <SessionProvider>
-              <QueryClientProvider client={queryClient}>
-                <Auth>{children}</Auth>
-              </QueryClientProvider>
-            </SessionProvider>
-          </ThemeProvider>
-        </AppProvider>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <AppProvider
+            branding={{ title: 'admin-dashboard-frontend' }}
+            navigation={NAVIGATION}
+            theme={theme}
+          >
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <SessionProvider>
+                <QueryClientProvider client={queryClient}>
+                  <Auth>{children}</Auth>
+                </QueryClientProvider>
+              </SessionProvider>
+            </ThemeProvider>
+          </AppProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
