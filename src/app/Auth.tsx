5c978b8d76d4ff42bf5f0cd9f0300900cbe16c53
mrviniciux <httpmarks@gmail.com>
2024-11-07 16:49:49 -0300

    feat(auth): simplified auth component

--- src/app/Auth.tsx
+++ src/app/Auth.tsx
@@ -2,20 +2,17 @@
 
 import { ReactNode } from 'react';
 import { useSession } from 'next-auth/react';
-import { usePathname, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function Auth({ children }: { children: ReactNode }) {
   const { data: session, status } = useSession();
   const router = useRouter();
-  const pathname = usePathname();
-
-  const protectedRoutes = ['/dashboard', '/profile'];
 
   if (status === 'loading') {
     return <p>Loading...</p>;
   }
 
-  if (!session && protectedRoutes.includes(pathname)) {
+  if (!session) {
     router.push('/api/auth/signin');
     return null;
   }
